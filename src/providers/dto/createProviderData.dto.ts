fef5bd84f457cf98810a2cccbcb3ddb22ba7075a
imdad <imdadadelabou0@gmail.com>
2025-08-16 13:26:19 +0100

    [FEAT] Add website to provider schema

--- src/providers/dto/createProviderData.dto.ts
+++ src/providers/dto/createProviderData.dto.ts
@@ -6,6 +6,7 @@ const CreateProviderDataSchema = z.object({
     .min(3, 'Business name is required')
     .max(100, 'Business name must be less than 100 characters'),
   description: z.string().min(3).optional(),
+  website: z.string().min(3).max(100, 'Website must be less than 100 characters').optional(),
   address: z.string().min(3).max(100, 'Address must be less than 100 characters').optional(),
   linkCode: z
     .string()
