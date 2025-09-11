f623df76ac35c2bff04df69689e1c315b8eb643f
imdad <imdadadelabou0@gmail.com>
2025-09-11 07:01:28 +0200

    FEAT

--- src/services/routes/scheduler/scheduler.route.ts
+++ src/services/routes/scheduler/scheduler.route.ts
@@ -2,9 +2,10 @@ import { Router } from 'express';
 import updateScheduler from '../../controllers/scheduler/update';
 
 const servicePath = '/service';
+const schedulerPath = '/scheduler';
 
 const schedulerRouter = Router();
 
-schedulerRouter.put(`${servicePath}/:serviceId`, updateScheduler);
+schedulerRouter.put(`${servicePath}/:serviceId${schedulerPath}`, updateScheduler);
 
 export default schedulerRouter;
