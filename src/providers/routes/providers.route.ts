b0baa423bdc6380e5c41a479d5705c0850ba405e
imdad <imdadadelabou0@gmail.com>
2025-09-11 06:43:35 +0200

    [FEAT] create the put to update a scheduler

--- src/providers/routes/providers.route.ts
+++ src/providers/routes/providers.route.ts
@@ -4,14 +4,18 @@ import me from '../controllers/me';
 import update from '../controllers/update';
 import createProviderService from '../../services/controllers/provider/createProviderService';
 import getProviderService from '../../services/controllers/provider/getProviderService';
+import schedulerRouter from '../../services/routes/scheduler/scheduler.route';
 
 const providersRouter = Router();
 
 const servicePath = '/service';
 
 providersRouter.post('/', create);
+
 providersRouter.post(servicePath, createProviderService);
 providersRouter.get(servicePath, getProviderService);
+providersRouter.use(schedulerRouter);
+
 providersRouter.get('/me', me);
 providersRouter.put('/me', update);
 
--- src/services/routes/scheduler/scheduler.route.ts
+++ src/services/routes/scheduler/scheduler.route.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import updateScheduler from '../../controllers/scheduler/update';
 
+const servicePath = '/service';
+
 const schedulerRouter = Router();
 
-schedulerRouter.put('/:serviceId', updateScheduler);
+schedulerRouter.put(`${servicePath}/:serviceId`, updateScheduler);
 
 export default schedulerRouter;
